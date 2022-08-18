adf63fd693ccdcb712a10775481be3d6d4954ec3
Vig_345 <72251437+vign2020@users.noreply.github.com>
2022-08-18 22:02:51 +0530

    Add files via upload

---
+++ j comp/script.js
@@ -0,0 +1,10 @@
+const counters=document.querySelectorAll('.counter');
+const speed=200;
+
+counters.forEach(counter=>{
+    const updateCount=()=>{
+        const target= +counter.getAttribute('data-target');
+        const count= +counter.innerText;
+        
+    }
+})
